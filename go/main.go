5e2c9753e43f73eb8a6b3ddc6b4f39f286cf002d
yuucu <yuucu.sub@gmail.com>
2022-07-20 23:47:50 +0900

    fix

--- go/main.go
+++ go/main.go
@@ -1256,7 +1256,7 @@ func bulkinsert01(c echo.Context, tx *sqlx.Tx, req []PostIsuConditionRequest, ji
 		valueArgs = append(valueArgs, jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
 	}
 	smt := fmt.Sprintf("INSERT INTO `isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES %s", strings.Join(valueHatena, ","))
-	_, err := tx.Exec(smt, valueArgs)
+	_, err := tx.Exec(smt, valueArgs...)
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
