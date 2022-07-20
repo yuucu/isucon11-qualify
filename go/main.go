5c7500f794758afda9f143e5e7a7003a8397997e
yuucu <yuucu.sub@gmail.com>
2022-07-21 03:30:00 +0900

    rm log

--- go/main.go
+++ go/main.go
@@ -211,9 +211,6 @@ func init() {
 func main() {
 	e := echo.New()
 
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
-	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.POST("/initialize", postInitialize)
